d75fe2ed5a434f6ddf214d94517bc452ee0975da
KaktusDaily <115354717+KaktusDaily@users.noreply.github.com>
2025-02-26 08:45:32 +0100

    Update app.php

--- app.php
+++ app.php
@@ -1,1 +1,11 @@
+<?php
+require 'db_connect.php';
 
+// Example Query: Fetch Users
+$stmt = $bdd->query("SELECT * FROM users");
+$users = $stmt->fetchAll();
+
+foreach ($users as $user) {
+    echo "User: " . $user['name'] . "<br>";
+}
+?>
