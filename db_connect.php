76be581d9969c8d721668e41d908920749d78111
KaktusDaily <115354717+KaktusDaily@users.noreply.github.com>
2025-02-26 08:43:28 +0100

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -1,1 +1,16 @@
+<?php
+try {
+    $bdd = new PDO(
+        "mysql:host=" . getenv("MYSQL_ADDON_HOST") . ";dbname=" . getenv("MYSQL_ADDON_DB") . ";port=" . getenv("MYSQL_ADDON_PORT"),
+        getenv("MYSQL_ADDON_USER"),
+        getenv("MYSQL_ADDON_PASSWORD"),
+        [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, // Enable exceptions for errors
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC, // Fetch results as associative arrays
+        ]
+    );
+} catch (PDOException $e) {
+    die("Database connection failed: " . $e->getMessage()); // Display error message
+}
+?>
 
